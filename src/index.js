fb0a0c433b45d8a72cfb9a7a0e8929bedd33a16d
Krzysztof Kosek <krzysiek.kosek@gmail.com>
2022-03-02 17:54:47 +0100

    Add store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import React from 'react';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.querySelector('#root')
 );
